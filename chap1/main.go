a33ff82240a21aa0ddbaf26f28a41cbed5c50d9a
Serge Lawson <iamsergelawson@gmail.com>
2025-02-05 09:55:02 +0000

    which(1) & intro to goroutine

--- chap1/main.go
+++ chap1/main.go
@@ -4,5 +4,12 @@ func main (){
 	// learning_switch()
 	// leaning_loop()
 	// standard_input()
-	 calculate_density()
-} 
+	// calculate_density()
+
+/* 	for i := 1; i < 4; i++{
+		go myPrint(i, 5)
+	}
+	time.Sleep(time.Second) */
+
+	which()
+ } 
---
+++ chap1/which.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func which(){
+	 arguments := os.Args
+
+	 if len(arguments) == 1 {
+		fmt.Println("Please enter aruments!")
+
+		return
+	 }
+
+	file := arguments[1]
+
+	 path := os.Getenv("PATH")
+
+	 pathList := filepath.SplitList(path)
+
+	 for _, directory := range pathList {
+
+		fullpath := filepath.Join(directory, file)
+
+		fileInfo, err := os.Stat(fullpath)
+
+		if err != nil {
+			continue
+		}
+
+		mode := fileInfo.Mode()
+
+		if !mode.IsRegular() {
+			continue
+		}
+
+		// Is it executable?
+		if mode&0111 != 0 {
+			fmt.Println(fullpath)
+			return
+		}
+	}
+
+	 
+}
